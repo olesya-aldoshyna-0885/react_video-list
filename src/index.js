fb7ff4d9d0486ced3309b9957c855c0d44c7c337
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-03-25 23:50:05 +0200

    activated component with video player

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Reader } from './components/Reader/Reader';
 import publications from './publications.json';
-// import { Example1 } from 'components/Example1';
+import { Example1 } from 'components/Example1';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Reader items={publications} />
- {/* < Example1 /> */}
+    < Example1 />
   </React.StrictMode>
 );
 
